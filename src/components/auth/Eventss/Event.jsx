5e695949b0f0ca084303090316792637c6567b5b
Othmane Assari <othmane.assari@gmail.com>
2024-07-01 18:48:47 +0200

    userprofilebackend

--- src/components/auth/Eventss/Event.jsx
+++ src/components/auth/Eventss/Event.jsx
@@ -14,12 +14,17 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 
 function Event() {
   const [eventTime, setEventTime] = useState(false);
-  const [eventDetails, setEventDetails] = useState(false);
+  const [eventDetail, setEventDetail] = useState(false);
   const [selectHour, setSelectHour] = useState(null);
+  const [calendarIsSmall, setCalendarIsSmall] = useState(false);
 
   const handleDateClick = (info) => {
     setEventTime((prev) => !prev);
     console.log(info.dateStr);
+  };
+  const handleBackButton = () => {
+    setEventTime(false);
+    setEventDetail(false);
   };
   const events = [
     { title: "", start: new Date() },
@@ -30,23 +35,26 @@ function Event() {
     <div>
       <div className="flex justify-center items-center bg-[#EEEEEE]">
         <div className="container-event py-0 bg-white rounded-lg px-0 w-[782px] h-full top-10 flex flex-col">
-          <div className="flex items-center justify-between w-full mb-4 my-3 mx-3">
-            <button className="bg-[#EEEFFC] w-[45px] h-[45px] rounded-full flex items-center justify-center overflow-hidden p-3">
+          <div className="flex items-center justify-between w-full p-3 px-5">
+            <button
+              onClick={handleBackButton}
+              className="bg-[#EEEFFC] w-[45px] h-[45px] rounded-full flex items-center justify-center overflow-hidden p-3"
+            >
               <img src={back} alt="" />
             </button>
-            <button className="flex items-center space-x-1 mr-5 p-5">
+            <button className="flex items-center space-x-1">
               <img src={link} alt="" />
               <p className="text-[13px] text-[#6572E1] underline">Copy link</p>
             </button>
           </div>
 
-          <div className="container-event py-0 bg-white border border-[#E2E8ED] rounded-lg  w-full h-fit flex flex-col">
+          <div className="container-event py-0 bg-white border border-[#E2E8ED] rounded-lg  w-full h-fit flex flex-col px-7">
             <div className="flex flex-col text-[#56647E] items-center">
               <h1 className="text-[28px]">Meeting IRM</h1>
               <h2>Setup Github repository Daily list of document</h2>
             </div>
             <hr className="horizontal-line-event my-4 m-auto"></hr>
-            <div className="flex items-center justify-between mx-8">
+            <div className="flex items-center justify-between">
               <div className="flex items-center space-x-1">
                 <img className="h-[20px] w-[20px]" src={user} alt="" />
                 <p className="text-[13px] text-[#919AAB] font-semibold">
@@ -69,57 +77,64 @@ function Event() {
               </div>
             </div>
             <hr className="horizontal-line-event my-4 mb-2 m-auto"></hr>
-            <div className="flex items-center justify-between mx-8">
-              <h1 className="text-[15px] pb-1">Select a Date & Time</h1>
-              <div className="flex items-center space-x-3">
-                <h1 className="text-[13px]">Zone Time</h1>
-                <button className="btn-google border p-4 flex items-center mx-2 bg-[#EEEFFC]">
-                  <h1 className="text-[13px] font-medium pb-1">
-                    Morocco Time (16:32)
-                  </h1>
-                  <img src={down} alt="" />
-                </button>
-              </div>
-            </div>
-            <div className="p-10 flex-grow overflow-auto">
-              {!eventTime && (
-                <FullCalendar
-                  events={events}
-                  plugins={[dayGridPlugin, interactionPlugin]}
-                  initialView="dayGridMonth"
-                  height="auto"
-                  contentHeight="auto"
-                  dateClick={handleDateClick}
-                />
-              )}
-              {eventTime ? (
-                <EventTime
-                  events={events}
-                  plugins={[dayGridPlugin, interactionPlugin]}
-                  setSelectHour={setSelectHour}
-                  selectHour={selectHour}
-                  dateClick={handleDateClick}
-                />
-              ) : (
-                <></>
-              )}
-              {/* /* <button onClick={() => setEventTime((prev) => !prev)}>
-                {!eventTime ? (
-                  <EventTime
-                    setSelectHour={setSelectHour}
-                    selectHour={selectHour}
-                  />
-                ) : (
-                  <FullCalendar
-                    events={events}
-                    plugins={[dayGridPlugin]}
-                    initialView="dayGridMonth"
-                    height="auto"
-                    contentHeight="auto"
-                  />
-                )}
-              </button> */}
-            </div>
+            {eventDetail ? (
+              <EventDetails
+                eventDetail={eventDetail}
+                setEventDetail={setEventDetail}
+              />
+            ) : (
+              <>
+                <div className="flex items-center justify-between">
+                  <h1 className="text-[15px] pb-1">Select a Date & Time</h1>
+                  <div className="flex items-center space-x-3">
+                    <h1 className="text-[13px]">Zone Time</h1>
+                    <button className="btn-google border p-4 flex items-center mx-2 bg-[#EEEFFC]">
+                      <h1 className="text-[13px] font-medium pb-1">
+                        Morocco Time (16:32)
+                      </h1>
+                      <img src={down} alt="" />
+                    </button>
+                  </div>
+                </div>
+                <div className="my-8 flex-grow overflow-auto flex">
+                  <div
+                    className={
+                      eventTime
+                        ? "event-calendar-small"
+                        : "event-calendar-large"
+                    }
+                  >
+                    <FullCalendar
+                      events={events}
+                      plugins={[dayGridPlugin, interactionPlugin]}
+                      initialView="dayGridMonth"
+                      height="auto"
+                      contentHeight="auto"
+                      dateClick={handleDateClick}
+                      setEventTime={setEventTime}
+                      eventTime={eventTime}
+                    />
+                  </div>
+
+                  {eventTime ? (
+                    <EventTime
+                      events={events}
+                      plugins={[dayGridPlugin, interactionPlugin]}
+                      setSelectHour={setSelectHour}
+                      selectHour={selectHour}
+                      dateClick={handleDateClick}
+                      setEventTime={setEventTime}
+                      eventTime={eventTime}
+                      setEventDetail={setEventDetail}
+                      eventDetail={eventDetail}
+                      onClick={handleBackButton}
+                    />
+                  ) : (
+                    <></>
+                  )}
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
